c56f6d7183dceffe5dd3fa76e7ce3d2074b2fd09
Manasa <manasa02manasa@gmail.com>
2023-02-09 20:16:37 +0530

    Minnor changes

--- js/payment.js
+++ js/payment.js
@@ -1,8 +1,10 @@
 function changeHandler(ev) {
 	var div = document.getElementById("payment-details");
+	var payButton = document.getElementById("pay-btn");
 	let val = ev.target.value;
 	if (val == "cod") {
 		div.style.display = "none";
+		payButton.innerText = "Order";
 	}
 	if (val == "card") {
 		div.style.display = "block";
